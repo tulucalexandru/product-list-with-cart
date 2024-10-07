a5f0d6aa24daa530fec19f008d1169e526741796
tulucalexandru <alexandrutuluc@alexanduslaptop.home>
2024-10-07 22:37:40 +0200

    add image component close #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AddToCart from "./components/buttons/AddToCart";
+import ImageCard from "./components/cards/ImageCard";
 
 function App() {
   return (
     <>
-      <AddToCart></AddToCart>
+      {/* <AddToCart></AddToCart> */}
+      <ImageCard />
     </>
   );
 }
