d65ff59eedb5c9e3e9cbade06338c270ca74c61d
tulucalexandru <tuluc.alexandru@gmail.com>
2024-10-10 23:40:19 +0200

    populate de product list from data.json
    closes #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import ProductCard from "./components/cards/ProductCard";
-import productList from './assets/data.json'
+import ProductList from "./components/layouts/ProductList";
 
 function App() {
   return (
     <>
       {/* <AddToCart></AddToCart> */}
       {/* <ImageCard /> */}
-      <ProductCard image={productList[0].image} name={productList[0].name} category={productList[0].category} price={productList[0].price.toString()} />
+      <ProductList />
     </>
-  );
+  )
+    ;
 }
 
 export default App;
